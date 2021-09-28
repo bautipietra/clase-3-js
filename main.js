078d8321e400869c452c92f1fc8fcd3cfb01659a
bautipietra <bautistapietraroia@gmail.com>
2021-09-28 11:22:37 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let numero = Number(prompt(`Ingresa un numero menor a 20 y te mostraremos los numeros que hay en el medio`));
+let numero = Number(prompt(`Ingresa un numero menor a 25 y te mostraremos los numeros que hay en el medio`));
 
-if (numero < 10) {
+if (numero < 25) {
     for (numero; numero < 25; numero++) {
         console.log(numero);
     }
 }
 else {
-    console.log(`El numero ingresado es mayor a 25`)
+    console.log(`El numero ingresado es mayor a 25.`)
 }
